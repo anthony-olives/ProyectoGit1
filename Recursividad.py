3152826d8425eb94dbf62b994961ce189046a6bd
Anthony <thony11michel@gmail.com>
2024-06-20 20:02:41 -0500

    Subida final

--- Recursividad.py
+++ Recursividad.py
@@ -1,3 +1,23 @@
-print("Comenta menta ")
-print("Hello")
-print("jsbkjbvkjxbv")
+'''
+# Metodo Recursivo
+def MostrarNumero(numero):
+    # Caso base
+    if numero <= 0:
+        return
+    # Caso Recursivo
+    else:
+        print(numero)
+        return MostrarNumero(numero-1)
+MostrarNumero(6)
+    '''
+    
+    
+def totalPaginas (Libros):
+    
+    if len(Libros) == 1:
+        return Libros [0]
+    else:
+        return Libros  + totalPaginas (Libros [1:]) 
+    
+print(totalPaginas ([10,20,30,40]))
+    
